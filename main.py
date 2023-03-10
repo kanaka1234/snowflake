5d44f0a8533fd56f6ce8c8b2523ec591a58400e2
kanaka1234 <gtkanaka6@gmail.com>
2023-03-10 16:05:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Import required libraries
+import snowflake.connector as sf
+
+# Connect to Snowflake
+conn = sf.connect(
+    user='kanakat',
+    password='Kanaka@123',
+    account='gabxoij-kyb59240',
+    warehouse='COMPUTE_WH',
+    database='MY_DB',
+    schema='MY_SCHEMA'
+)
+
+# Execute SQL statement and perform data transformation
+result = ''
+cursor = conn.cursor()
+cursor.execute("INSERT INTO  MY_TABLE(name,age,add) values('Kanaka',32,'Bangalore');")
+rows = cursor.fetchall()
+
+print(rows)
+
+cursor.close()
+conn.close()
